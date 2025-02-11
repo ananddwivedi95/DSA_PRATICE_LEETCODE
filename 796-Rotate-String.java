6bded66dc8275ee6578589155fbae7a500ee0c85
Anand Dwivedi <133527439+ananddwivedi95@users.noreply.github.com>
2025-02-11 22:58:36 +0530

    [796] [Time Beats: 22.36%] [Memory Beats: 51.07%] - LeetPush

--- 796-Rotate-String.java
+++ 796-Rotate-String.java
@@ -1,41 +1,71 @@
 class Solution {
     public boolean rotateString(String s1, String s2) {
-         // Your code here
-        // StringBuilder sb= new StringBuilder(s2);
-        
-        
-        // //check right rotation
-        // int i=0;
-        // while(i<sb.length())
-        // {
-        //     char ch=sb.charAt(0);
-        //     sb.deleteCharAt(0);
-        //     sb.append(ch);
-        //     if(s1.equals(sb.toString()))
-        //     {
-        //         return true;
-        //     }
-        //     i++;
-        // }
-        // //check left rotation
-        // int j=sb.length()-1;
-        // while(j>=0)
-        // {
-        //  char ch=sb.charAt(sb.length()-1);
-        //  sb.deleteCharAt(sb.length()-1);
-        //  sb.insert(0,ch);
-        //  if(s1.equals(sb.toString()))
-        //  {
-        //      return true;
-        //  }
-        //  j--;
-        // }
-        // return false;
         if(s1.length()!=s2.length())
         {
             return false;
         }
-        s1=s1+s1;
-        return s1.contains(s2);
+        int lps[]= new int[s2.length()]; //lps array
+        findLps(lps,s1); //compute lps
+        return areRotationUsingKmp(s2+s2,s1,lps); //using kmp algorithm
+        
+    }
+    static boolean areRotationUsingKmp(String text,String pat,int[] lps)
+    {
+        int n=text.length(); //length of text
+        int m=pat.length(); //length of pattern
+        int i=0; //track text character
+        int j=0; //track pattern character
+        while(i<text.length())
+        {
+            if(text.charAt(i)==pat.charAt(j))
+            {
+                i++;
+                j++;
+                if(j==m)
+                {
+                    return true;
+                }
+            }
+            else
+            {
+                if(j==0)
+                {
+                    i++;
+                }
+                else
+                {
+                    j=lps[j-1];
+                }
+            }
+        }
+        
+        return false;
+    }
+    
+    static void findLps(int []lps,String s1)
+    {
+        int p=0; //prefix
+        int s=1; //suffix
+        while(s<s1.length())
+        {
+            if(s1.charAt(p)==s1.charAt(s)) //character match
+            {
+                lps[s]=p+1;
+                s++;
+                p++;
+            }
+            else //character not match
+            {
+                if(p==0) // if p==0
+                {
+                    lps[s]=0;
+                    s++;
+                }
+                else
+                {
+                    p=lps[p-1]; //if p!=0 then p=lps[p-1]
+                }
+            }
+        }
     }
 }
