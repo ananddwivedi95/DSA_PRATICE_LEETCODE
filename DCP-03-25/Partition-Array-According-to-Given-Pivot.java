830fcdb628ea03df72aa5dbb69410eda70cf7e40
Anand Dwivedi <133527439+ananddwivedi95@users.noreply.github.com>
2025-03-03 12:03:50 +0530

    [2161] [Time Beats: 85.39%] [Memory Beats: 15.73%] - LeetPush

--- DCP-03-25/Partition-Array-According-to-Given-Pivot.java
+++ DCP-03-25/Partition-Array-According-to-Given-Pivot.java
@@ -1,58 +1,48 @@
 class Solution {
     public int[] pivotArray(int[] arr, int pivot) {
-       int ii=0;
-       ArrayList<Integer> smallest=new ArrayList<>();
-       ArrayList<Integer> mid=new ArrayList<>();
-       ArrayList<Integer> greater= new ArrayList<>();
-       while(ii<arr.length)
-       {
-           if(arr[ii]<pivot)
-           {
-               smallest.add(arr[ii]);
-               ii++;
-           }
-           else if(arr[ii]==pivot)
-           {
-               mid.add(arr[ii]);
-               ii++;
-           }
-           else if(arr[ii]>pivot)
-           {
-               greater.add(arr[ii]);
-               ii++;
-           }
+        int ii=0;
+        int lessCount=0;
+        int equalCount=0;
+        while(ii<arr.length)
+        {
+            if(arr[ii]<pivot)
+            {
+                lessCount++;
 
-       }
-      int [] result= new int[arr.length];
-       int i=0; //for smallest ArrayList
-       int j=0; // for mid ArrayList
-       int k=0; //for greater ArrayList
-       int index=0;
-       int sLength=smallest.size();
-       int mLength=mid.size();
-       int gLength=greater.size();
-       int n=result.length;
-       while(index<n)
-       {
-           if(index<sLength)
-           {
-               result[index]= smallest.get(i);
-               index++;
-               i++;
-           }
-           else if(index<sLength+mLength)
-           {
-               result[index]= mid.get(j);
-               index++;
-               j++;
-           }
-           else if(index<n)
-           {
-               result[index]= greater.get(k);
-               index++;
-               k++;
-           }
-       }
+            }
+            if(arr[ii]==pivot)
+            {
+                equalCount++;
+
+            }
+            ii++;
+        }
+        ii=0;
+        int [] result=new int[arr.length];
+        int i=0;
+        int j=lessCount;
+        int k=lessCount+equalCount;
+        while(ii<arr.length)
+        {
+            if(arr[ii]<pivot)
+            {
+                result[i]=arr[ii];
+                i++;
+                ii++;
+            }
+            else if(arr[ii]==pivot)
+            {
+                result[j]=arr[ii];
+                j++;
+                ii++;
+            }
+            else if(arr[ii]>pivot)
+            {
+                result[k]=arr[ii];
+                k++;
+                ii++;
+            }
+        }
         return result;
     }
 }
