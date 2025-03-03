e03da307509f49450a1f44750e498395c5cb02a4
Anand Dwivedi <133527439+ananddwivedi95@users.noreply.github.com>
2025-03-03 11:26:22 +0530

    [2161] [Time Beats: 28.09%] [Memory Beats: 77.96%] - LeetPush

--- DCP-03-25/Partition-Array-According-to-Given-Pivot.java
+++ DCP-03-25/Partition-Array-According-to-Given-Pivot.java
@@ -1,51 +1,58 @@
 class Solution {
     public int[] pivotArray(int[] arr, int pivot) {
-        int i=0;
+       int ii=0;
        ArrayList<Integer> smallest=new ArrayList<>();
        ArrayList<Integer> mid=new ArrayList<>();
        ArrayList<Integer> greater= new ArrayList<>();
-       while(i<arr.length)
+       while(ii<arr.length)
        {
-           if(arr[i]<pivot)
+           if(arr[ii]<pivot)
            {
-               smallest.add(arr[i]);
-               i++;
+               smallest.add(arr[ii]);
+               ii++;
            }
-           else if(arr[i]==pivot)
+           else if(arr[ii]==pivot)
            {
-               mid.add(arr[i]);
-               i++;
+               mid.add(arr[ii]);
+               ii++;
            }
-           else if(arr[i]>pivot)
+           else if(arr[ii]>pivot)
            {
-               greater.add(arr[i]);
-               i++;
+               greater.add(arr[ii]);
+               ii++;
            }
 
        }
-       int [] result= new int[arr.length];
+      int [] result= new int[arr.length];
+       int i=0; //for smallest ArrayList
+       int j=0; // for mid ArrayList
+       int k=0; //for greater ArrayList
        int index=0;
-       i=0; //smallest
-        while(i<smallest.size())
-        {
-            result[index]=smallest.get(i);
-            index++;
-            i++;
-        }
-        i=0;//mid
-        while(i<mid.size())
-        {
-            result[index]=mid.get(i);
-            index++;
-            i++;
-        }
-        i=0;//greater
-        while(i<greater.size())
-        {
-            result[index]=greater.get(i);
-            index++;
-            i++;
-        }
+       int sLength=smallest.size();
+       int mLength=mid.size();
+       int gLength=greater.size();
+       int n=result.length;
+       while(index<n)
+       {
+           if(index<sLength)
+           {
+               result[index]= smallest.get(i);
+               index++;
+               i++;
+           }
+           else if(index<sLength+mLength)
+           {
+               result[index]= mid.get(j);
+               index++;
+               j++;
+           }
+           else if(index<n)
+           {
+               result[index]= greater.get(k);
+               index++;
+               k++;
+           }
+       }
         return result;
     }
 }
