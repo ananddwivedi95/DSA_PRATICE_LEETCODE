3e486fd156158663a80f77b76ebcfbae46929e37
Anand Dwivedi <133527439+ananddwivedi95@users.noreply.github.com>
2025-03-25 23:20:11 +0530

    [34] [Time Beats: 100.00%] [Memory Beats: 67.78%] - LeetPush

---
+++ Find-First-and-Last-Position-of-Element-in-Sorted-Array.java
@@ -0,0 +1,55 @@
+class Solution {
+    public int[] searchRange(int[] arr, int target) {
+      \tint lb=lowerBound(arr,target);
+        int ub=upperBound(arr,target);
+        int [] result={-1,-1};
+       if (lb == arr.length || arr[lb] != target) {   
+           return result;
+    }
+        result[0]=lb;
+        result[1]=ub-1;
+        return result;
+    }
+    static int lowerBound(int[] arr,int target)
+    {
+        int left=0;
+        int right=arr.length-1;
+        int result=arr.length;
+        while(left<=right)
+        {
+            int mid=left+(right-left)/2;
+            if(target<=arr[mid])
+            {
+                result=Math.min(result,mid);
+                right=mid-1;
+            }
+            else if(target>arr[mid])
+            {
+                left=mid+1;
+            }
+
+        }
+        return result;
+    }
+    static int upperBound(int[] arr,int target)
+    {
+        int left=0;
+        int right=arr.length-1;
+        int result=arr.length;
+        while(left<=right)
+        {
+            int mid=left+(right-left)/2;
+            if(target<arr[mid])
+            {
+                result=Math.min(result,mid);
+                right=mid-1;
+            }
+            else if(target>=arr[mid])
+            {
+                left=mid+1;
+            }
+
+        }
+        return result;
+    }
+}
